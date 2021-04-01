4818f73f78f201df8832b2a668202b55ab7a8a29
Jesús Vélez <jvelezmagic@gmail.com>
2021-04-01 12:57:17 -0600

    Initial commit.

---
+++ titanic_platzi/utils/paths.py
@@ -0,0 +1,51 @@
+from pyhere import here
+from pathlib import PosixPath
+from typing import (
+    Union,
+    Callable,
+    Iterable,
+)
+
+def make_dir_function(
+    dirname: Union[str, Iterable[str]]
+) -> Callable[..., PosixPath]:
+    """Generate a fucntion that converts a string or iterable of strings into
+    a path relative to the project directory.
+
+    Args:
+        dirname: Name of the subdirectories to extend the path of the main
+            project.
+            If an iterable of strings is passed as an argument, then it is
+            collapsed to a single steing with anchors dependent on the
+            operating system.
+
+    Returns:
+        A function that returns the path relative to a directory that can
+        receive `n` number of arguments for expansion.
+    """
+
+    def dir_path(*args) -> PosixPath:
+        if isinstance(dirname, str):
+            return here(dirname, *args)
+        else:
+            return here(*dirname, *args)
+
+    return dir_path
+
+project_dir = here
+
+for dir_type in [
+        ["data"],
+        ["data", "raw"],
+        ["data", "processed"],
+        ["data", "interim"],
+        ["data", "external"],
+        ["models"],
+        ["notebooks"],
+        ["references"],
+        ["reports"],
+        ["reports", "figures"],
+        ["titanic_platzi"]
+    ]:
+    dir_var = '_'.join(dir_type) + "_dir"
+    exec(f"{dir_var} = make_dir_function({dir_type})")
